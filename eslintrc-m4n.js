30601131da5cb22faa87e33daccc7d16c879414b
Mechazawa <mega@ioexception.at>
2021-10-02 01:01:02 +0200

    init

---
+++ eslintrc-m4n.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: '@mapcreator',
+  rules: {
+    'prefer-destructuring': 'off',
+    'max-depth': 'off',
+    'max-params': 'off',
+    'max-len': 'off',
+    'max-lines': 'off',
+    'max-lines-per-function': 'off',
+    'import/no-webpack-loader-syntax': 'off',
+  },
+  overrides: [
+    {
+      files: ['*CM.vue', '*SVG*.vue', '*WYSIWYG*.vue'],
+      rules: {
+        'vue/match-component-file-name': 'off',
+      },
+    },
+  ],
+};
